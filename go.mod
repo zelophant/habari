b621594a1965e9e29ed2d6b2ddab3f18924b998a
zelophant <mattbiggs95@gmail.com>
2024-07-12 23:31:39 +0100

    websocket stuff - not in working state. see console log

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zelophant/habari
 
 go 1.22.4
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		for {
+			// Read message from client
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to client
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.Handle("/", http.FileServer(http.Dir("./src")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
